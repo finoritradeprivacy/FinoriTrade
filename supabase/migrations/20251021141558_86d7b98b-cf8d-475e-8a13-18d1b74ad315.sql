daad7e19958394cc66c6b5506c4bc6596ccd12e1
finoritradeprivacy <finoritrade.privacy@gmail.com>
2025-12-29 22:17:46 +0100

    fix flags and config

--- supabase/migrations/20251021141558_86d7b98b-cf8d-475e-8a13-18d1b74ad315.sql
+++ supabase/migrations/20251021141558_86d7b98b-cf8d-475e-8a13-18d1b74ad315.sql
@@ -9,7 +9,7 @@ SELECT cron.schedule(
   '*/1 * * * *', -- Every minute (pg_cron limitation)
   $$
   SELECT net.http_post(
-    url:='https://agsgsdjobfpodjneknpm.supabase.co/functions/v1/update-asset-prices',
+    url:='https://vcorpphtywkrrpaenywh.supabase.co/functions/v1/update-asset-prices',
     headers:='{"Content-Type": "application/json", "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnc2dzZGpvYmZwb2RqbmVrbnBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MTQwNzYsImV4cCI6MjA3NjA5MDA3Nn0.pMILTRvjd2ZLvPgcIjx1TwHQ4RsVwddepC4vQqmZKn0"}'::jsonb,
     body:='{}'::jsonb
   ) AS request_id;
--- supabase/migrations/20251213092118_de4d6692-db07-47d4-a3fc-388d447e93ab.sql
+++ supabase/migrations/20251213092118_de4d6692-db07-47d4-a3fc-388d447e93ab.sql
@@ -4,7 +4,7 @@ SELECT cron.schedule(
   '0 * * * *',
   $$
   SELECT net.http_post(
-    url := 'https://agsgsdjobfpodjneknpm.supabase.co/functions/v1/generate-news',
+    url := 'https://vcorpphtywkrrpaenywh.supabase.co/functions/v1/generate-news',
     headers := jsonb_build_object(
       'Content-Type', 'application/json',
       'Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnc2dzZGpvYmZwb2RqbmVrbnBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MTQwNzYsImV4cCI6MjA3NjA5MDA3Nn0.pMILTRvjd2ZLvPgcIjx1TwHQ4RsVwddepC4vQqmZKn0'
